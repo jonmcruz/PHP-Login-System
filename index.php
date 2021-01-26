2a9104e7982a0e3a136af57ae12923b7b79e8cb7
Jonathan Cruz <jonathan.m.cruz1996@gmail.com>
2021-01-26 17:30:00 -0500

    Added base HTML layout to page

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<?php echo: "Hello World!" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="robots" content="follow" />
+
+	<title>Page Title</title>
+
+	<base href="/" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.14/dist/css/uikit.min.css" />
+</head>
+
+<body>
+
+	<div class="uk-section uk-container uk-text-center">
+		Test
+	</div>
+
+	<!-- jQuery might be required, but for now lets try without:
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	-->
+
+	<!-- UIKit JS -->
+	<script src="https://cdn.jsdelivr.net/npm/uikit@3.6.14/dist/js/uikit.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/uikit@3.6.14/dist/js/uikit-icons.min.js"></script>
+
+</body>
+</html>
