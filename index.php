20fcd9ef7a04aa78f2a3e4e67d3c0f931a4821ab
Jonathan Cruz <jonathan.m.cruz1996@gmail.com>
2021-01-26 18:16:57 -0500

    Added login form

--- index.php
+++ index.php
@@ -14,8 +14,30 @@
 
 <body>
 
-	<div class="uk-section uk-container uk-text-center">
-		Test
+	<div class="uk-section uk-container">
+		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+			<form class="uk-form-stacked">
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password...">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			    	<button class="uk-button uk-button-default" type="submit">Login</button>
+			    </div>
+
+			</form>
+		</div>
 	</div>
 
 	<!-- jQuery might be required, but for now lets try without:
