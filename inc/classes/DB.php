2a297d11a9c01ef5747bed993f63a5888da172ce
Jonathan Cruz <jonathan.m.cruz1996@gmail.com>
2021-01-27 18:28:44 -0500

    Added DB file for database connection

---
+++ inc/classes/DB.php
@@ -0,0 +1,36 @@
+<?php
+
+//If there is no constant defined called __CONFIG__, do not load this file
+if(!defined('__CONFIG__')) {
+	exit('You do not have a config file');
+}
+
+class DB {
+
+	protected static con;
+
+	private function __construct() {
+
+		try {
+
+			self::$con = new PDO( 'mysql:charset=utf8mb4;host=localhost;port=3306;dbname=login_course', 'root', 'root' );
+			self::$con->setAttribute( PDO::ATTR_ERRMODE, PDO:ERRMODE_EXCEPTION );
+			self::$con->setAttribute( PDO::ATTR_PERSISTENT, false );
+
+		} catch (PDOException $e) {
+			echo "Could not connect to the database."; exit;
+		}
+	}
+
+	public static function getConnection() {
+
+		//If this instance has not been started, start it.
+		if (!self::$con) {
+			new DB();
+		}
+	
+		//Return the writable db connection
+		return self::$con;
+	}
+}
+?>
