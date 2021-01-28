47001c7b4d881afb930101d35074303ddbc06edb
Jonathan Cruz <jonathan.m.cruz1996@gmail.com>
2021-01-27 19:42:38 -0500

    Added additional error messages for form fields on registration page

--- inc/footer.php
+++ inc/footer.php
@@ -5,3 +5,5 @@
 <!-- UIKit JS -->
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.14/dist/js/uikit.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.14/dist/js/uikit-icons.min.js"></script>
+
+<script src="/assets/js/main.js"></script>
